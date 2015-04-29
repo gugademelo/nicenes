770a99d9f9b082424361902fbf8dae1a82ce00a1
unknown <rm69464@L2701MICRO06.fiap.com.br>
2015-04-28 21:07:48 -0300

    update classe livro

--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,5 +1,146 @@
 package model;
 
-public class Livro {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
 
+import database.ConnectionFactory;
+
+public class Livro {
+	private Integer livro_id;
+	private String titulo, isbn, colecao, edicao, idioma, assunto;
+	private Date ano;
+	private String autor;
+	private String editora;
+	private String categoria;
+	
+	public Livro()
+	{
+	
+	}
+	
+	public Livro(int livro_id ,String titulo, String isbn, String colecao, String edicao, String idioma, String assunto, Date ano, String autor, String editora, String categoria)
+	{
+		this.livro_id = livro_id;
+		this.titulo = titulo;
+		this.isbn = isbn;
+		this.colecao = colecao;
+		this.edicao = edicao;
+		this.idioma = idioma;
+		this.assunto = assunto;
+		this.ano = ano;
+		this.autor = autor;
+		this.editora = editora;
+		this.categoria = categoria;
+		
+	}
+	
+	public boolean inserirLivro() {
+		Connection con = new ConnectionFactory().getConnection();
+		
+		if(con == null){
+			return false;	
+		}
+
+		String sql = "INSERT INTO livro (titulo, isbn, colecao, edicao, idioma, assunto, ano, autor, editora, categoria) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, this.getTitulo());
+			st.setString(2, this.getIsbn());
+			st.setString(3, this.getColecao());
+			st.setString(1, this.getEdicao());
+			st.setString(2, this.getIdioma());
+			st.setString(3, this.getAssunto());
+			//terminar
+			if(st.executeUpdate() == 1) return true;
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+				
+	}
+	
+	
+	public Integer getLivro_id() {
+		return livro_id;
+	}
+
+	public void setLivro_id(Integer livro_id) {
+		this.livro_id = livro_id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getColecao() {
+		return colecao;
+	}
+	public void setColecao(String colecao) {
+		this.colecao = colecao;
+	}
+	public String getEdicao() {
+		return edicao;
+	}
+	public void setEdicao(String edicao) {
+		this.edicao = edicao;
+	}
+	public String getIdioma() {
+		return idioma;
+	}
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+	public Date getAno() {
+		return ano;
+	}
+	public void setAno(Date ano) {
+		this.ano = ano;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
 }
