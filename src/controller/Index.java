1b0c35651668c478e8c98c20d6d6b7bf5451e2ca
Gustavo Melo <gcastromelo94@gmail.com>
2015-05-24 12:00:44 -0300

    arrumei a treta do web.xml

--- src/controller/Index.java
+++ src/controller/Index.java
@@ -17,7 +17,7 @@ import model.Usuario;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/nicenes")
+@WebServlet("/home")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
